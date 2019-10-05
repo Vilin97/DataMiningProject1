374be1fa41edf7c38cc14dea5faa5e9e41f2231d
Vasily <vasilin97@gmail.com>
2019-10-05 12:43:17 -0400

    Just starting out

---
+++ kmeans.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import sys
+
+
+path = sys.argv[1]
+n = sys.argv[2]
+k = sys.argv[3]
+
+data = pd.read_csv(path+"movies.csv")
+
+def transform(data): # one hot encode and select features and data points
+    return
+
+def kmeans(n,k):
+    return
